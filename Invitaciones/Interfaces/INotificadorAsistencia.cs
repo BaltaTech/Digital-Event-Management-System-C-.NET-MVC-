defe0badafb60bd45163ea3ada103d4a3be3adf4
Saul Baltazar <chrisnxtch@gmail.com>
2026-03-13 12:29:50 -0600

    Remove comment from INotificadorAsistencia interface
    Removed comment about the observer pattern.

--- Invitaciones/Interfaces/INotificadorAsistencia.cs
+++ Invitaciones/Interfaces/INotificadorAsistencia.cs
@@ -2,7 +2,6 @@
 
 namespace Invitaciones.Interfaces
 {
-    // Esta es la base para cualquier "Observador" que quiera saber si alguien confirmó
     public interface INotificadorAsistencia
     {
         void Actualizar(Invitado invitado);
