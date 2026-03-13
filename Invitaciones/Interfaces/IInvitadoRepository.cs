8da4dfb290b5631fb153cc5aeccac180bd2dbe9b
Saul Baltazar <chrisnxtch@gmail.com>
2026-03-13 12:27:40 -0600

    Remove commented lines from IInvitadoRepository

--- Invitaciones/Interfaces/IInvitadoRepository.cs
+++ Invitaciones/Interfaces/IInvitadoRepository.cs
@@ -5,8 +5,6 @@ namespace Invitaciones.Interfaces
     public interface IInvitadoRepository
     {
         void Guardar(Invitacion invitacion);
-
-        // Añadimos estos dos métodos obligatorios
         void Actualizar(Invitado invitado);
         IEnumerable<Invitado> ObtenerTodos();
     }
